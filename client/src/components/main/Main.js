0958b745338ff8cd231463bfba39c359e08a5916
Alex Tkachenko <alextkachenco1233211@gmail.com>
2023-05-27 17:22:39 +0300

    Initiated Main

---
+++ client/src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css'
+
+const Main = ({text}) => {
+    return (
+        <main className={'main'}>
+            Main {text}
+        </main>
+    );
+};
+
+export default Main;
