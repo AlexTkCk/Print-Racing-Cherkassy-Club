904b07d4c35d3cf89566787e76705c701cce037f
Alex Tkachenko <alextkachenco1233211@gmail.com>
2023-05-27 17:19:40 +0300

    Imported Header, Main, Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Header from './components/header/Header'
+import Main from './components/main/Main'
+import Footer from './components/footer/Footer'
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+        <Header title={'Title'}>
 
+        </Header>
+
+        <Main text={'Title'}>
+
+        </Main>
+        <Footer text={'Title'}>
+        </Footer>
     </div>
-  );
+    );
 }
 
 export default App;
