ed9bd42fd45f7e4cdbf39ddcda58a84ea79d6f34
Alex Tkachenko <alextkachenco1233211@gmail.com>
2023-05-27 18:24:02 +0300

    Created toggling title cursor

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Header.css'
 
 const Header = ({}) => {
+
+    const [titleContent, setTitleContent] = useState('Print');
+    const [titleCursorClass, setTitleCursorClass] = useState('');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTitleCursorClass(prevState => prevState === 'active' ? '' : 'active');
+        }, 500);
+
+        return () => {
+            clearInterval(interval);
+        }
+    });
+
+
     return (
         <header className={'header'}>
-            <h1 className={'header__title'}>Print Racing</h1>
+            <h1 className={'header__title'}>
+                <div className={`header__title_cursor ${titleCursorClass}`}>
+                </div>
+                {titleContent}
+            </h1>
         </header>
     );
 };
