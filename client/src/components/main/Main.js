bfd2433b29e57bb9b90a7924cf6adc7ee9e80ac6
Alex Tkachenko <alextkachenco1233211@gmail.com>
2023-05-27 21:56:45 +0300

    Initiate hidden textarea, user input displaying block

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import randomWords from 'random-words';
 import './Main.css'
 
+const rndText = randomWords(100).join(' ');
+
 const Main = ({inGame, logInGameHandler}) => {
+
+    const [usersInput, setUsersInput] = useState('');
+
     return inGame ?
         (
             <main className={'main'}>
-                <button onClick={logInGameHandler}></button>
-                Gaming
+                <div className="main__random_text_container">
+                    {rndText}
+                    <div className="main__users_input_container">
+                        {usersInput}
+                    </div>
+                    <textarea onChange={e => {
+                        setUsersInput(e.target.value);
+                    }} name="textarea" className={'main__textarea_hidden'}></textarea>
+                </div>
             </main>
         )
         :
