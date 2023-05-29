580d519d6cdaff5b860b1b36530eae67650e4e3c
Alex Tkachenko <alextkachenco1233211@gmail.com>
2023-05-29 10:45:26 +0300

    Fixed pop up displaying due to connection Created log in form to connect to game

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -8,23 +8,36 @@ const Main = ({inGame, logInGameHandler}) => {
 
     const [popUpVisible, setPopUpVisible] = useState('');
     const [popUpText, setPopUpText] = useState('');
+    const [currentUserId, setCurrentUserId] = useState('You will appear here');
+    const [randomText, setRandomText] = useState('');
 
     useEffect(() => {
-        const socket = io('ws://localhost:5000');
-
-        socket.on('client_connected', (data) => {
+        if (inGame) {
             setPopUpVisible('popUp_active');
             setPopUpText(
                 <>
                     <h1 className={'popup__title'}>Welcome !</h1>
-                    <h2 className={'popUp__subTitle'}>{data.client_id}</h2>
+                    <h2 className={'popUp__subTitle'}>{currentUserId}</h2>
                 </>
             );
 
             setTimeout(() => {
                 setPopUpVisible('');
             }, 2000);
+        }
+    }, [inGame])
+    useEffect(() => {
+        const socket = io('ws://localhost:5000');
+
+        socket.on('random_text_generated', (data) => {
+            setRandomText(data.random_text);
         })
+
+        socket.on('client_connected', (data) => {
+            setCurrentUserId(data.client_id);
+        })
+
+        socket.emit('get_random_text')
 
         return (() => {
             socket.disconnect()
@@ -54,8 +67,28 @@ const Main = ({inGame, logInGameHandler}) => {
         :
         (
             <main className={'main'}>
-                <button onClick={logInGameHandler}></button>
-                Not gaming
+
+                <div className="main__log_in_container">
+                    <h1 className="log_in__title">
+                       Your unique
+                        <div className={'title__description_hover'}>
+                            <div className="ID_description__container">
+                                <p className="description_container__text">
+                                    This ID should be user by other people to start game with you.
+                                </p>
+                            </div>
+                            ID
+                        </div>
+                    </h1>
+                    <h2 className="log_in__subTitle">
+                        {currentUserId}
+                    </h2>
+
+                    <input type="text" className="log_in__input" placeholder={'Input room ID'}/>
+
+                    <button className="log_in__input_button" onClick={logInGameHandler}>Log in -></button>
+                </div>
+
             </main>
         );
 };
