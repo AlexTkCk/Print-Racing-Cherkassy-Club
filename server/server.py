6a2d0fc7252156b1cac72efed6c72302c43c5ade
Clown <avidmenko3@gmail.com>
2023-06-04 02:08:36 +0300

    upd handle_connect_to_client handle_connect added handle_check_text

--- server/server.py
+++ server/server.py
@@ -13,37 +13,75 @@ def generate_random_text():
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
-
 connected_clients = set()
+rooms_data = {}
+
+
 @socketio.on('connect_to_client')
-def handle_connect_to_client(data):
-    client_id = data['client_id']
-    join_room(client_id)  #
-    emit('connected_to_client', {'client_id': client_id})
+def handle_connect_to_client(connectid):
+    room = connectid
+    if room not in connected_clients:
+        return emit('room_not_found', {'room_id': connectid})
+
+    join_room(room)
+    if room not in rooms_data:
+        rooms_data[room] = []  # Создание списка для каждой комнаты
+    text = generate_random_text()
+    rooms_data[room].append(text)
+    emit('connected_to_client', {'room_id': connectid})
+
 
 @socketio.on('disconnect_from_client')
-def handle_disconnect_from_client(data):
-    client_id = data['client_id']
+def handle_disconnect_from_client():
+    client_id = request.sid
     leave_room(client_id)
     emit('disconnected_from_client', {'client_id': client_id})
+
 
 @socketio.on('connect')
 def handle_connect():
     client_id = request.sid
+    room = client_id
     connected_clients.add(client_id)
-    join_room(client_id)
+    join_room(room)
+    if room not in rooms_data:
+        rooms_data[room] = []  # Создание списка для каждой комнаты
+    text = generate_random_text()
+    rooms_data[room].append(text)
     emit('client_connected', {'client_id': client_id})
+
 
 @socketio.on('disconnect')
 def handle_disconnect():
     client_id = request.sid
     connected_clients.remove(client_id)
+    leave_room(client_id)
     emit('client_disconnected', {'client_id': client_id}, broadcast=True)
+
 
 @socketio.on('get_random_text')
 def handle_get_random_text():
     random_text = generate_random_text()
     emit('random_text_generated', {'text': ' '.join(random_text)})
+
+@socketio.on('check_text')
+def handle_check_text(data):
+    room = data['room']
+    entered_text = data['text']
+    generated_text = rooms_data.get(room)
+
+    if generated_text is None:
+        return emit('check_text_result', {'status': 400})
+
+    if len(entered_text) != len(generated_text):
+        return emit('check_text_result', {'status': 400})
+
+    for entered_char, generated_char in zip(entered_text, generated_text):
+        if entered_char != generated_char:
+            return emit('check_text_result', {'status': 400})
+
+    return emit('check_text_result', {'status': 200})
+
 
 if __name__ == '__main__':
     socketio.run(app, port=5000)
