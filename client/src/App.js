dbf295e0a092209fad1ef3543c0920d5012825e8
Alex Tkachenko <alextkachenco1233211@gmail.com>
2023-06-05 11:45:57 +0300

    Deleted unnecessary handlers from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,19 @@ import './App.css';
 import Header from './components/header/Header'
 import Main from './components/main/Main'
 import Footer from './components/footer/Footer'
-import {useState} from "react";
 
 function App() {
 
-    const [inGame, setInGame] = useState(false);
-
-    const logInGameHandler = () => {
-        setInGame((prevState) => !prevState)
-    }
-
     return (
         <div className="App">
-        <Header title={'Title'}>
+        <Header>
 
         </Header>
 
-        <Main text={'Title'} inGame={inGame} logInGameHandler={logInGameHandler}>
+        <Main>
 
         </Main>
-        <Footer text={'Title'}>
+        <Footer>
         </Footer>
     </div>
     );
