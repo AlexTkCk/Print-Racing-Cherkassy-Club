9c7e8acdb24f7daa32a45ef59f241b55abc16bc3
Alex Tkachenko <alextkachenco1233211@gmail.com>
2023-06-11 16:30:59 +0300

    Moved socket id out of main.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,12 @@
 import { io } from 'socket.io-client';
 
 const socket = io('ws://localhost:5000');
+const currentUserId = await new Promise((resolve) => {
+    socket.on('client_connected', (data) => {
+        resolve(data.client_id);
+    })
+})
 
-export {socket};
+
+export {socket, currentUserId};
 
